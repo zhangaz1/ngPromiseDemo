fcf31a6dbb08d763f46df4e9bc8fe3c69d764eac
zhangaz1 <zhangaz1@hotmail.com>
2016-07-28 10:20:03 +0800

    链式then的走法

--- src/js/mainCtrl.js
+++ src/js/mainCtrl.js
@@ -24,10 +24,38 @@
         return void(0);
 
         function promiseDemo() {
-            $q.when(getData())
+            var promise = $q.when(getData());
+
+            promise.finally(function() {
+                $log.log('finally:', arguments);
+            });
+
+            promise.then(function() {
+                    $log.log('then 2:', arguments);
+                })
                 .then(function() {
-                    $log.log(arguments);
+                    $log.log('then 2.1:', arguments);
                 });
+
+            promise.then(function() {
+                    $log.log('then 1:', arguments);
+                })
+                .then(function() {
+                    $log.log('then 1.1:', arguments);
+                });
+
+            promise.then(function() {
+                $log.log('then 3:', arguments);
+                return $q.reject(arguments);
+            }).then(function() {
+                $log.log('then resolve 3.1:', arguments);
+            }, function() {
+                $log.log('then reject 3.1:', arguments);
+            }).then(function() {
+                $log.log('then resolve 3.2:', arguments);
+            }, function() {
+                $log.log('then reject 3.2:', arguments);
+            });
         }
 
         function getData() {
